03ce03efbcfbad8a810f0b00a32e5a72540bc919
hitcodemann <89034654+hitcodemann@users.noreply.github.com>
2025-02-13 12:45:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function analyzeRepo() {
         return;
     }
 
-    fetch("https://zekibdxnrk.execute-api.us-west-2.amazonaws.com/dev/travel-advice", { 
+    fetch("https://zekibdxnrk.execute-api.us-west-2.amazonaws.com/dev/github-analysis", { 
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -32,37 +32,5 @@ function analyzeRepo() {
     .catch(error => {
         console.error("Error fetching analysis:", error);
         document.getElementById("analysisResult").innerText = "Error fetching analysis. Try again.";
-    });
-}
-
-function getAdvice() {
-    let country = document.getElementById("country").value;
-    if (!country) {
-        alert("Please enter a country!");
-        return;
-    }
-
-    fetch("https://zekibdxnrk.execute-api.us-west-2.amazonaws.com/dev/travel-advice", { 
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ 
-            type: "travel",
-            country: country 
-        })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return response.json();
-    })
-    .then(data => {
-        document.getElementById("advice").innerText = data.advice;
-    })
-    .catch(error => {
-        console.error("Error fetching advice:", error);
-        document.getElementById("advice").innerText = "Error fetching advice. Try again.";
     });
 }
